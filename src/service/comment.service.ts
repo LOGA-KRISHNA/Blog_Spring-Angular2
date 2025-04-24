e259f490d34f55f900663838125e1c864f7c6784
LOGA-KRISHNA <logakrishnad@gmail.com>
2025-04-24 08:30:57 +0530

    i think last commit

--- src/service/comment.service.ts
+++ src/service/comment.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const url="https://blog-springbootbackend-50026124227.development.catalystappsail.in";
+
 @Injectable({
   providedIn: 'root'
 })
