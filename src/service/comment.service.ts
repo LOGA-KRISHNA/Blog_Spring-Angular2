479a0e1e6bb85bf1d53096f348ed5b8bef191eb9
LOGA-KRISHNA <logakrishnad@gmail.com>
2025-05-13 20:07:12 +0530

    this is the end of thanos

--- src/service/comment.service.ts
+++ src/service/comment.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
@@ -10,12 +10,20 @@ export class CommentService {
   constructor(private http: HttpClient) { }
 
   createComment(post_id: number, postedBy: string, content: string): Observable<any> {
-    const params = {
-      postId: post_id.toString(),
-      postedBy: postedBy,
-      content: content
-    };
-    return this.http.post(`${API_URL}/comment/create`,{ params });
+    // const params = {
+    //   postId: post_id,
+    //   postedBy: postedBy,
+    //   content: content
+    // };
+    // console.log(params);
+    
+    // return this.http.post(`${API_URL}/comment/create`, {params} );
+    const params = new HttpParams()
+    .set('post_id', post_id.toString())
+    .set('postedBy', postedBy)
+    .set('content', content);
+
+  return this.http.post(`${API_URL}/comment/create`, null, { params });
   }
 
   getAllPostById(post_id: number): Observable<any> {
